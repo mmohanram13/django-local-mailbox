8104ae9e278a44bb630f06bf21a3a68cef05280d
Mohan Ram M <mmohanram13@gmail.com>
2018-09-13 18:13:31 +0530

    Update settings.py

--- mailClient/mailClient/settings.py
+++ mailClient/mailClient/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mailclient',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
